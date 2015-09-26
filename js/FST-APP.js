75d0f0ec02b02b014fa4268e1ae5a88fe937e03b
Camilo Barbosa <barbosa93cg@gmail.com>
2015-09-26 13:25:09 -0500

    Super Lite

--- js/FST-APP.js
+++ js/FST-APP.js
@@ -1,61 +1,89 @@
-function isJQ(html,html2)
+
+
+
+var ArrFastAppi=[];
+
+
+
+function isJQ(html, html2)
 {
-	return $(html).is(html2);
+    return $(html).is(html2);
 }
 function isReady(html)
-{	
-	return isJQ(html,":visible");	
-}
-function ob(activ,slave,nactiv,cactiv,nslave)
 {
-   if(isReady(activ))
-   {
-   $(activ).click(function(){
-              for(var x=1;isReady(nactiv+x+"")||isJQ(nactiv+x+"",":not(:visible)");x++)
-              {
-                  $(nactiv+x+"").fadeIn();
-                  if((nactiv+x+"")!==cactiv)$(nslave+x+"").slideUp();
-              }
-              $(slave).slideDown();
-              $(this).hide();
-              $(activ+"_on").slideDown();
-              $(activ+"_off").slideUp();
-              
-   });
-   if(isJQ(activ,".activ_on"))$(activ).click();
-   if(isJQ(activ,".getText"))
-   {
-       for(var i=1;i<=6;i++)
-       {
-           if(isJQ(slave,".h_"+i+""))
-           {
-               $(slave).prepend("<h"+i+">"+$(activ).text()+"</h"+i+">");
-           }
-       }
-   }
-   
-   
-   }
-   return;
-   
+    return isJQ(html, ":visible");
 }
-function fast_appi(activ,slave)
+function looking(html)
 {
-    this.activ=activ;
-    this.slave=slave;    
-    this.run=function()
+    return isReady(html)||isJQ(html,":not(:visible)");
+}
+
+
+
+function ob(activ, slave, nactiv, nslave, id, fps)
+{
+    if (isReady(activ))
     {
-       var nactiv="."+this.activ;
-       var nslave="."+this.slave;
-       
-      for(var i=1;isReady("."+this.activ+i+"");i++)
-      {
-          
-          var cactiv="."+this.activ+i+"";
-          var cslave="."+this.slave+i+"";
-          $(cslave).hide();
-          ob(cactiv,cslave,nactiv,cactiv,nslave);   
-          
-      }
+        $(activ).click(function () {
+                 
+            $(ArrFastAppi[fps].activ + ArrFastAppi[fps].id + "").fadeIn();
+            $(ArrFastAppi[fps].slave + ArrFastAppi[fps].id + "").slideUp();     
+
+            $(slave).slideDown();
+            $(this).hide();
+            $(activ + "_on").slideDown();
+            $(activ + "_off").slideUp();
+            ArrFastAppi[fps].activ = nactiv;
+            ArrFastAppi[fps].slave = nslave;
+            ArrFastAppi[fps].id = id;
+
+        });
+        if (isJQ(activ, ".activ_on"))
+            $(activ).click();
+        if (isJQ(activ, ".getText"))
+        {
+            for (var i = 1; i <= 6; i++)
+            {
+                if (isJQ(slave, ".h_" + i + ""))
+                {
+                    $(slave).prepend("<h" + i + ">" + $(activ).text() + "</h" + i + ">");
+                }
+            }
+        }
+
+
+    }
+    return;
+
+}
+function fast_appi(activ, slave)
+{
+    this.activ = activ;
+    this.slave = slave;
+    
+    var fp={activ:"."+this.activ, slave:"."+this.slave,id:0};
+    var z=0;
+    
+    for(var i=0;i<ArrFastAppi.length;i++)
+        if(ArrFastAppi[i].activ===ob.activ)
+            z++;
+    
+    if(!z)ArrFastAppi.push(fp);
+    this.fps=ArrFastAppi.length -1;
+
+    this.run = function ()
+    {
+        var nactiv = "." + this.activ;
+        var nslave = "." + this.slave;
+
+        for (var i = 1; isReady("." + this.activ + i + ""); i++)
+        {
+
+            var cactiv = "." + this.activ + i + "";
+            var cslave = "." + this.slave + i + "";
+            $(cslave).hide();
+            ob(cactiv, cslave, nactiv, nslave, i,this.fps);
+
+        }
     };
 }
